095402f3d8c5a59fb77edccef8f081d2ce23224a
stacey46 <staceyjepkemoi@gmail.com>
2025-09-01 11:13:51 +1000

    Added the report page

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -14,11 +14,11 @@ interface Product {
 // Map product names to real images
 const productImages: { [key: string]: string } = {
   Banana:
-    "https://media.istockphoto.com/id/1291262112/photo/banana.webp?a=1&b=1&s=612x612&w=0&k=20&c=KSmjx5R8Qk4mzzGw8tAP3CAPoSH_zdXD7sZYd1lBuD4=",
+    "https://images.unsplash.com/photo-1587334206596-c0f9f7dccbe6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fGJhbmFuYXxlbnwwfHwwfHx8MA%3D%3D",
   Apple:
     "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?ixlib=rb-4.0.3&auto=format&fit=crop&w=1024&q=80",
   Cucumber:
-    "https://media.istockphoto.com/id/967491012/photo/whole-with-slice-cucumber-isolated-on-white-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=LOEDxL1YeoPLmSqOGtwt-IBSYWYhMI7QKgAsqBByG1Q=",
+    "https://images.unsplash.com/photo-1449300079323-02e209d9d3a6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3VjdW1iZXJ8ZW58MHx8MHx8fDA%3D",
   Tomato:
     "https://plus.unsplash.com/premium_photo-1726138646616-ec9fb0277048?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dG9tYXRvZXN8ZW58MHx8MHx8fDA%3D",
   Carrot:
